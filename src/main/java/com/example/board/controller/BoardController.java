1df254876d8b2db550cf365b2a5b871b7f83cdb8
TakanoriShima <quark2galaxy@gmail.com>
2022-11-01 13:35:30 +0900

    19. 掲示板を作成する - 3

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.board.repository.Post;
 import com.example.board.repository.PostFactory;
 import com.example.board.repository.PostRepository;
+import com.example.board.validation.GroupOrder;
 
 /**
  * 掲示板のフロントコントローラー.
@@ -62,7 +63,9 @@ public class BoardController {
 	 * @return テンプレート
 	 */
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	public String create(@ModelAttribute("form") @Validated Post form, BindingResult result, Model model) {
+//	public String create(@ModelAttribute("form") @Validated Post form, BindingResult result, Model model) {
+	public String create(@ModelAttribute("form") @Validated(GroupOrder.class) Post form, BindingResult result,
+			Model model) {
 		if (!result.hasErrors()) {
 			repository.saveAndFlush(PostFactory.createPost(form));
 			model.addAttribute("form", PostFactory.newPost());
@@ -96,7 +99,9 @@ public class BoardController {
 	 * @return テンプレート
 	 */
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String update(@ModelAttribute("form") @Validated Post form, BindingResult result, Model model) {
+//	public String update(@ModelAttribute("form") @Validated Post form, BindingResult result, Model model) {
+	public String update(@ModelAttribute("form") @Validated(GroupOrder.class) Post form, BindingResult result,
+			Model model) {
 		if (!result.hasErrors()) {
 			Optional<Post> post = repository.findById(form.getId());
 			repository.saveAndFlush(PostFactory.updatePost(post.get(), form));
